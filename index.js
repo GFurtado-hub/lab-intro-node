2f68290a1d757e8960f29dd7f4144b912c86f9a9
Gonçalo Furtado <gfurtado.sw@gmail.com>
2024-12-10 20:30:13 +0000

    done something

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+this.items.push(item);
+this.items.sort((a,b) => a-b);
+this.length = this.length + 1;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[this.length - 1];
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this
+  }
 
   sum() {}
 
